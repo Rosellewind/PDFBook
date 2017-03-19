609b7a4de4140e8b7fcea321f33cb4fdd594f841
Roselle Tanner <roselletanner@roselles-macbook.local>
2017-03-19 11:41:00 -0600

    Initial Commit

---
+++ PDFBook/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PDFBook
+//
+//  Created by Roselle Tanner on 3/19/17.
+//  Copyright © 2017 Roselle Tanner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PDFBook/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PDFBook
+//
+//  Created by Roselle Tanner on 3/19/17.
+//  Copyright © 2017 Roselle Tanner. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
